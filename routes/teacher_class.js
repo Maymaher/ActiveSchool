f21becc36909c325495e419cf47379958e4110c5
Maymaher <mai.maher003@gmail.com>
2021-06-18 14:53:16 -0400

    feature(feat-teacher-apis) adding rest teacher apis

--- routes/teacher_class.js
+++ routes/teacher_class.js
@@ -8,12 +8,13 @@ var classes=require("../models/class");
 // var teacherClass=require("../models/teacher_class");
 
 
+//get all teachers and thier classes
 router.get('/',async (req, res)=> {
   const teachers= await teacherClass.find().populate("tclass").populate("teacher");
   res.send(teachers);
 });
 
-
+//post teacher id and his class number
 router.post('/',async (req, res)=> {
   const tclass=req.body.tclass;  
   const teacher=req.body.teacher;  
@@ -30,6 +31,28 @@ router.post('/',async (req, res)=> {
     res.send("error occured while saving")
 })
 });
+
+//get teacher with id 
+router.get("/:id", async (req, res) => {
+	const teacher = await teacherClass.find({ teacher: req.params.id }).populate("tclass").populate("teacher")
+	res.send(teacher)
+})
+
+
+//delete specific class for one teacher
+router.delete("/", async (req, res) => {
+  const tclass=req.body.tclass;  
+  const teacher=req.body.teacher;  
+	try {
+		await teacherClass.deleteOne({ teacher: teacher,tclass:tclass })
+		res.status(204).send()
+	} catch {
+		res.status(404)
+		res.send({ error: "teacher doesn't exist!" })
+	}
+})
+
+
   
   module.exports = router;
   
