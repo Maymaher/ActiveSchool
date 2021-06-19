1ea8ce98183181de9387f066dddc0b5d72b6eda6
emansoliman <emyrade29@gmail.com>
2021-06-19 08:39:29 -0400

    feat-Authontication using Pasport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
 var router = express.Router();
+const  passport  =  require('passport');
+const  LocalStrategy  =  require('passport-local').Strategy;
+
 
 
 var indexRouter = require('./routes/index');
@@ -58,9 +61,20 @@ app.use('/material', matrialRouter);
 
 
 app.use('/teacherclass', teacherClassRouter);
+
 // app.use('/getclasses', teacherClass);
 
 
+//Authontication
+// const auth = () => {
+//   return (req, res, next) => {
+//       next()
+//   }
+// }
+
+// app.post('/authenticate', auth() , (req, res) => {
+//   res.status(200).json({"statusCode" : 200 ,"message" : "hello"});
+// });
 
 
 
@@ -83,6 +97,52 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//Auth
+const auth = () => {
+  return (req, res, next) => {
+      passport.authenticate('local', (error, user, info) => {
+          if(error) res.status(400).json({"statusCode" : 200 ,"message" : error});
+          req.login(user, function(error) {
+              if (error) return next(error);
+              next();
+          });
+      })(req, res, next);
+  }
+}
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      if(username === "admin" && password === "admin"){
+          return done(null, username);
+      } else {
+          return done("unauthorized access", false);
+      }
+  }
+));
+
+
+
+passport.serializeUser(function(user, done) {
+  if(user) done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next()
+    }
+    return res.status(400).json({"statusCode" : 400, "message" : "not authenticated"})
+}
+
+app.get('/getData', isLoggedIn, (req, res) => {
+  res.json("data")
+})
 const MONGO_URL = "mongodb://localhost:27017/ActiveSchoolDB";
 mongoose.connect(
   MONGO_URL,
