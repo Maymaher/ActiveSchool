53acbc0e560b362f26906427e14e98e1bce86d29
emansoliman <emyrade29@gmail.com>
2021-06-20 08:49:51 -0400

    feat-Autontication add hash password Function

--- models/user.js
+++ models/user.js
@@ -19,7 +19,38 @@ class:{type:mongoose.Schema.Types.ObjectId,ref:'Class',required:false},
 
 
 });
+///////////////
 
+userschema.pre('save', function (next) {
+    var user = this;
+    if (this.isModified('password') || this.isNew) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if (err) {
+                return next(err);
+            }
+            bcrypt.hash(user.password, salt, null, function (err, hash) {
+                if (err) {
+                    return next(err);
+                }
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        return next();
+    }
+});
+
+
+/////
+userschema.methods.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, function (err, isMatch) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
 
 userModel = mongoose.model('User', userschema);
 
